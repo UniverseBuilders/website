b68d142e907239a742bfa836116ccc404680675f
Tylar Murray <murray.tylar@gmail.com>
2015-03-19 10:19:29 -0400

    working minimal example

--- app.js
+++ app.js
@@ -7,9 +7,7 @@ var app = angular.module('ng-boot-boiler-demo',
     [
         require('ui.bootstrap'),
         require('ngTouch'),
-        require('header-navbar'),
-        require('splash-header'),
-        require('app-footer')
+        require('splash-header')
     ], function($httpProvider){
         FastClick.attach(document.body);
         delete $httpProvider.defaults.headers.common['X-Requested-With'];
